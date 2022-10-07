07a0b2bf16e59acc9673ef43abe4819af30cec90
Elijah Kariuki <ElijahKariuki1981@outlook.com>
2022-10-07 11:23:53 -0400

    added calculations for area when given the radius

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -13,7 +13,15 @@
             if(a == 17 && b == 4)
             {
                 Console.WriteLine($"{a}/{b} is {quotient} with a remainder of {remainder}");
+               
             }
+            Console.WriteLine(AreaOfCircle());
+        }
+        public static double AreaOfCircle()
+        {
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            return Math.PI * radius * radius;
         }
     }
 }
